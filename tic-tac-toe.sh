f8b0d458eadf7f40869c0be9700721204f53739b
krzkro4122 <krolkrzysztof1999@gmail.com>
2022-11-13 22:51:09 +0100

    Winning now can take place.

--- tic-tac-toe.sh
+++ tic-tac-toe.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# GLOBAL VARIABLES
 FIELDS=(1 2 3 4 5 6 7 8 9)
-WINS=("1 2 3" "4 5 6" "7 8 9" "1 4 7" "2 5 8" "3 6 9" "1 5 9" "3 5 7")
-TURN='O'
-WINNER=0
 HISTORY=()
+TURN='O'
+# Win tracking containers
+# O
+O_ROWS=(0 0 0)
+O_COLUMNS=(0 0 0)
+O_DIAGONAL=(0 0 0)
+O_OPPOSITE_DIAGONAL=(0 0 0)
+# X
+X_ROWS=(0 0 0)
+X_COLUMNS=(0 0 0)
+X_DIAGONAL=(0 0 0)
+X_OPPOSITE_DIAGONAL=(0 0 0)
 
 function is_field_empty () {
     # Check if the field is empty
@@ -34,11 +44,45 @@ function move () {
         result=$?
     done
 
-    # Append the move to current player's and global history
+    # Append the move to the global history
+    HISTORY+=("${field}")
+
+    # Populate the corresponding win tracking containers
+    which_row=$(( (field - 1) / 3 ))
+    which_column=$(( field % 3 ))  # doesn't actually matter if this is the exact collumn just has to be consistent
+    diagonal_index=$(( field / 4 ))
+    opposite_diagonal_index=$(( (field / 2) - 1 ))
     if [[ ${TURN} == 'O' ]]; then
-        HISTORY+=("${field}")
+        # The move is always on SOME column and SOME row
+        O_ROWS[${which_row}]=$(( O_ROWS[which_row] + 1 ))
+        O_COLUMNS[${which_column}]=$(( O_COLUMNS[which_column] + 1 ))
+        # Diagonal
+        if (( field % 4 == 1 )); then
+            O_DIAGONAL[${diagonal_index}]=1
+        fi
+        # Opposite diagonal
+        if (( field == 3 || field == 5 || field == 7 )); then
+            O_OPPOSITE_DIAGONAL[${opposite_diagonal_index}]=1
+        fi
+        echo "O_ROWS: [${O_ROWS[@]}]"
+        echo "O_COLUMNS: [${O_COLUMNS[@]}]"
+        echo "O_DIAGONAL: [${O_DIAGONAL[@]}]"
+        echo "O_OPPOSITE_DIAGONAL: [${O_OPPOSITE_DIAGONAL[@]}]"
     else
-        HISTORY+=("${field}")
+        X_ROWS[${which_row}]=$(( X_ROWS[which_row] + 1 ))
+        X_COLUMNS[${which_column}]=$(( X_COLUMNS[which_column] + 1 ))
+        # Diagonal
+        if (( field % 4 == 1 )); then
+            X_DIAGONAL[${diagonal_index}]=1
+        fi
+        # Opposite diagonal
+        if (( field == 3 || field == 5 || field == 7 )); then
+            X_OPPOSITE_DIAGONAL[${opposite_diagonal_index}]=1
+        fi
+        echo "X_ROWS: [${X_ROWS[@]}]"
+        echo "X_COLUMNS: [${X_COLUMNS[@]}]"
+        echo "X_DIAGONAL: [${X_DIAGONAL[@]}]"
+        echo "X_OPPOSITE_DIAGONAL: [${X_OPPOSITE_DIAGONAL[@]}]"
     fi
 }
 
@@ -80,46 +124,80 @@ function draw () {
     done
 }
 
-function find_winner() {
-    for win in "${WINS[@]}";
-    do
-        echo ${win}
-        for position in win;
-        do
-            echo ${1} | grep position
-        # if [ ${?} != 0 ]; then
-        #     break
-        # fi
-        done
-    done
-    echo ""
-    # for i in ${1};
-    # do
-    #     echo ${i}
-    # done
-}
-
+# if anybody wins this returns 1, else it returns 0
 function check_win() {
-    echo "Current player: ${TURN}"
-    player_history=()
-
-    printf "%s: {" ${TURN}
-    if [[ "${TURN}" == "O" ]]; then
-        rest=0
-    else
-        rest=1
-    fi
-
-    for i in ${!HISTORY[@]};
-    do
-        if (( i % 2 == rest )); then
-            printf "%d, " ${HISTORY[$i]}
-            player_history+=("${HISTORY[$i]}")
+    # Determine who's win trackers to check
+    if [[ ${TURN} == 'O' ]]; then
+        # Check rows
+        for value in ${O_ROWS[@]};
+        do
+            if (( value == 3 )); then
+                return 1
+            fi
+        done
+        # Check columns
+        for value in ${O_COLUMNS[@]};
+        do
+            if (( value == 3 )); then
+                return 1
+            fi
+        done
+        # Check the diagonal
+        aggregate=0
+        for value in ${O_DIAGONAL[@]};
+        do
+            aggregate=$(( aggregate + value ))
+        done
+        if (( aggregate == 3 )); then
+            return 1
         fi
-    done
-    echo "}"
-
-    find_winner ${player_history}
+        # Check the opposite diagonal
+        aggregate=0
+        for value in ${O_OPPOSITE_DIAGONAL[@]};
+        do
+            aggregate=$(( aggregate + value ))
+        done
+        if (( aggregate == 3 )); then
+            return 1
+        fi
+    else
+        # Check rows
+        for value in ${X_ROWS[@]};
+        do
+            if (( value == 3 )); then
+                return 1
+            fi
+        done
+        # Check columns
+        for value in ${X_COLUMNS[@]};
+        do
+            if (( value == 3 )); then
+                return 1
+            fi
+        done
+        # Check the diagonal
+        aggregate=0
+        for value in ${X_DIAGONAL[@]};
+        do
+            aggregate=$(( aggregate + value ))
+        done
+        echo "aggregate: ${aggregate}"
+        if (( aggregate == 3 )); then
+            return 1
+        fi
+        # Check the opposite diagonal
+        aggregate=0
+        for value in ${X_OPPOSITE_DIAGONAL[@]};
+        do
+            aggregate=$(( aggregate + value ))
+        done
+        echo "aggregate: ${aggregate}"
+        if (( aggregate == 3 )); then
+            return 1
+        fi
+    fi
+    # if nothing found then return 0
+    return 0
 }
 
 function main () {
@@ -129,9 +207,15 @@ function main () {
         move
         draw
         check_win
+        winner=${?}
+        if (( winner == 1 )); then
+            echo "Player ${TURN} WON!"
+            # Delete the save file here?
+            exit 0
+        fi
         end_turn
     done
+    echo "NOBODY won this round!"
 }
 
-# main
-find_winner
+main
